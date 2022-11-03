1cdfe66d63186f1371978374984d3aab891e909f
Dmytro Chumachenko <104909647+chumachenkoo@users.noreply.github.com>
2022-11-03 13:38:24 +0100

    Added 2 models UserView and QRCodeView

--- qrcode_project/qr_generator/models.py
+++ qrcode_project/qr_generator/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+class UserView(models.Model):
+    username = models.CharField(max_length=20, null=False)
+    email = models.EmailField(null=False)
+    password = models.CharField(max_length=25, null=False)
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
+class QRCodeView(models.Model):
+    qr_code = models.BinaryField(null=False)
+    owner = models.ForeignKey(UserView, on_delete=models.CASCADE)
+
+    objects = models.Manager()
+
+    class Meta:
+        verbose_name = 'QRCode'
+        verbose_name_plural = 'QRCodes'
