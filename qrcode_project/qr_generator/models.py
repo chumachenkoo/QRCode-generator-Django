c4a98c09513d92a88a22c43d40780c292b5a4a77
Dmytro Chumachenko <104909647+chumachenkoo@users.noreply.github.com>
2022-11-04 15:07:22 +0100

    added registration, login, logout, account, generator features, added generate and decode methods in QRCode class, also fixed front part with qr codes on account page

--- qrcode_project/qr_generator/models.py
+++ qrcode_project/qr_generator/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+import qrcode
+from io import BytesIO
+import base64
 
 # Create your models here.
 
@@ -24,3 +27,25 @@ class QRCodeView(models.Model):
     class Meta:
         verbose_name = 'QRCode'
         verbose_name_plural = 'QRCodes'
+
+    @staticmethod
+    def generate(data, user_id):
+        image = qrcode.make(data)
+        buffer = BytesIO()
+        image.save(buffer, format='png')
+
+        inf = base64.b64encode(buffer.getvalue())
+
+        user = UserView.objects.get(id=user_id)
+
+        qr = QRCodeView(qr_code=inf, owner=user)
+        return qr
+
+    @staticmethod
+    def decode(user):
+        result = []
+        for data in user.qrcodeview_set.all():
+            qr = data.qr_code
+            info = f'data:image/png;base64,{qr.decode("UTF-8")}'
+            result.append(info)
+        return result
--- qrcode_project/qr_generator/views.py
+++ qrcode_project/qr_generator/views.py
@@ -1,23 +1,64 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponseNotFound, HttpResponseBadRequest, JsonResponse, HttpResponseForbidden
-from django.template.response import TemplateResponse
+from django.http import HttpResponseRedirect
+from .models import UserView, QRCodeView
 
 
 def registration(request):
+    if 'id' in request.session:
+        return HttpResponseRedirect('/account')
+
+    elif request.method == 'POST':
+        UserView.objects.create(username=request.POST.get('username'),
+                                email=request.POST.get('email'),
+                                password=request.POST.get('password'))
+
+        return render(request, 'login.html')
+
     return render(request, 'registration.html')
 
 
 def login(request):
+    if 'id' in request.session:
+        return HttpResponseRedirect('/account')
+
+    elif request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = UserView.objects.get(email=email)
+
+        if user is not None and password == user.password:
+            request.session['id'] = user.id
+            return HttpResponseRedirect('/account')
+
     return render(request, 'login.html')
 
 
 def logout(request):
-    return HttpResponseRedirect('/login')
+    request.session.flush()
+    return render(request, 'login.html')
 
 
 def account(request):
-    return render(request, 'account.html')
+    if 'id' in request.session:
+        user_id = request.session['id']
+        user = UserView.objects.get(id=user_id)
+        qr = QRCodeView.decode(user=user)
+
+        return render(request, 'account.html', {'data': user, 'qr_codes': qr})
+
+    return render(request, 'login.html')
 
 
 def generator(request):
-    return render(request, 'generator.html')
+    if 'id' in request.session:
+        if request.method == 'POST':
+            data = request.POST.get('qr_code')
+            user_id = request.session['id']
+            qr = QRCodeView.generate(data, user_id)
+            qr.save()
+
+            return HttpResponseRedirect('/account')
+        else:
+            return render(request, 'generator.html')
+
+    return HttpResponseRedirect('/login')
